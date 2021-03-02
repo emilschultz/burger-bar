6f10c6ba04803185b6adaa5c46de03347cb27a7b
Emil Schultz <emilrschultz@gmail.com>
2021-03-02 13:38:12 +0100

    Routing and Sign up page

---
+++ pages/signup.js
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+
+export default function signup() {
+  return(
+    <>
+      <h1>Sign up here</h1>
+      <form>
+        <label>E-mail</label>
+        <input type="text" placeholder="e-mail" name="e-mail" required></input>
+
+        <label>Password</label>
+        <input type="text" placeholder="password" name="password"></input>
+
+        <button type="submit">Create user</button>
+        <Link href="/">
+          <button type="button">Cancel</button>
+        </Link>
+
+      </form>
+    </>
+  )
+}
