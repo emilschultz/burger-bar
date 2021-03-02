d1601eaf01b671f2c7879af0b6cd9fb0d16d874a
Emil Schultz <emilrschultz@gmail.com>
2021-03-02 13:52:48 +0100

    login page

--- pages/login.js
+++ pages/login.js
@@ -1,8 +1,23 @@
+import Link from 'next/link';
 
 export default function login() {
   return(
     <>
       <h1>Login</h1>
+      <form>
+        <label htmlFor="e-mail">E-mail</label>
+        <input type="text" placeholder="e-mail" name="e-mail" required></input>
+        
+        <label htmlFor="password">Password</label>
+        <input type="text" placeholder="password" name="password" required></input>
+
+        <button type="submit">Login</button>
+        <Link href="/">
+          <button type="button">Cancel</button>
+        </Link>
+
+
+      </form>
     </>
   )
 }
