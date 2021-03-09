b365ad3651b20949c8c456410c0f81fa3d25979a
Emil Schultz <emilrschultz@gmail.com>
2021-03-09 22:03:57 +0100

    initial cart function. Not functioning well

--- pages/burgers.js
+++ pages/burgers.js
@@ -1,11 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import firebase from '../config/firebase';
+import { useCart } from '../context/CartContext';
+
 import NavBar from '../components/NavBar';
 import GlobalStyle from '../components/GlobalStyle';
 
 function Burgers() {
 
-  let [burgers, setBurgers] = useState([]); 
+  const [burgers, setBurgers] = useState([]); 
+  const cart = useCart();
+  console.log('The cart:', cart);
+
+  
 
   useEffect(()=>{
     firebase.firestore().collection('burgers')
@@ -19,8 +25,14 @@ function Burgers() {
       <div key={burger.id}>
         <h1>{burger.name}</h1>
         <p>{burger.description}</p>
-        <p>{burger.price}</p>
-        <button>+</button>
+        <p>{burger.price} kr</p>
+        <button onClick={() => {
+          cart.addProductToCart({
+            title: `${burger.name}`,
+            price: `${burger.price}`
+          })
+        }}>
+          +</button>
       </div> 
     );
   });
@@ -33,6 +45,16 @@ function Burgers() {
         <h1>Burgers</h1>
           {burgerList}
         <h1>Cart</h1>
+        <ul>
+          {cart.productsInCart.map((item) => {
+            return (
+              <li>
+                {item.title} - {item.price}
+              </li>
+            )
+          })}
+        </ul>
+        <p>Total: {cart.total}</p>
       </main>
     </>
   )
