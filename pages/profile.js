b5c7a0fb59b0a758642552a22276fd79af9196f1
Emil Schultz <emilrschultz@gmail.com>
2021-03-12 10:54:16 +0100

    feat: authentication

--- pages/profile.js
+++ pages/profile.js
@@ -1,9 +1,10 @@
-
 import React, { useEffect } from "react";
 
 import firebase from "../config/firebase";
 import { useAuth } from "../config/auth";
 import { useRouter } from "next/router";
+import { firebaseAdmin } from "../config/firebaseAdmin";
+import nookies from "nookies";
 import GlobalStyle from "../components/GlobalStyle";
 
 export default function Profile() {
@@ -21,19 +22,40 @@ export default function Profile() {
 
   return (
     <div>
-    <GlobalStyle />
-    <div>
-      <p>Profile</p>
-      <button onClick={handleSignout}>Sign out</button>
-      {userContext && (
-        <>
-          <h1>Username:</h1>
-          <p>{userContext.email}</p>
-          <h1>Password:</h1>
-          <p>{userContext.uid}</p>
-        </>
-      )}
-    </div>
+      <GlobalStyle />
+      <div>
+        <p>Profile</p>
+        <button onClick={handleSignout}>Sign out</button>
+        {userContext && (
+          <>
+            <h1>Username:</h1>
+            <p>{userContext.email}</p>
+            <h1>Password:</h1>
+            <p>{userContext.uid}</p>
+          </>
+        )}
+      </div>
     </div>
   );
+}
+
+export const getServerSideProps = async (ctx) => {
+  try {
+    const cookies = nookies.get(ctx);
+    const token = await firebaseAdmin.auth().verifyIdToken(cookies.token);
+    console.log(cookies.token);
+    console.log("TOKEN", JSON.stringify(token));
+    const { uid, email } = token;
+    return {
+      props: { email, uid },
+    };
+  } catch (error) {
+    console.log("Something went WRONG!");
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/login",
+      },
+    };
+  }
 };
