63153b511afcd7afb3368913a18c62831b246c1a
Emil Schultz <emilrschultz@gmail.com>
2021-03-18 12:43:51 +0100

    Responsive grid for menu components

---
+++ components/SectionGrid/index.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const SectionGrid = styled.section`
+  width: 100vw;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(320px, 2fr));
+  grid-template-rows: auto;
+  grid-gap: 1rem;
+
+  div {
+    grid-column: span 1;
+    justify-self: center;
+  }
+`;
+
+export default SectionGrid;
